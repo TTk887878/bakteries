a61617261f644b5c4248ef7b0b37a3b20bcf364c
TTk887878 <maximusx39@gmail.com>
2025-01-26 18:15:25 +0300

    add new rrrrr new chanchesss

--- bacteries/clientt.py
+++ bacteries/clientt.py
@@ -1,9 +1,55 @@
 import socket
 import time
+import pygame
+import math
 
-sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 1.семейство адресов ipv4 2.тип подключения
+
+def drow_text(x, y, r, text, color):
+    font = pygame.font.Font(None, r)
+    text = font.render(text, True, color)
+    rect = text.get_rect(center=(x,y))
+    screen.blit(text,rect)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 1.семейство адресов ipv4 2.тип подключения
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY,
-                   1)  # 3. выбрали парамметр и отключили пакетированиеб для отсуцтвия задержки
+                1)  # 3. выбрали парамметр и отключили пакетированиеб для отсуцтвия задержки
 sock.connect(("localhost", 10000))  # 4.привзывае подкл к определенному серверу и порту
-while True:
-    sock.send("привет".encode())
+
+pygame.init()
+name = "амебиус"
+
+WIDTH = 800
+HEIGHT = 700
+CC = (WIDTH // 2, HEIGHT // 2)
+old = (0, 0)
+radius = 50
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("амёба VS амёба")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if pygame.mouse.get_focused():
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            vector = pos[0] - CC[0], pos[1] - CC[1]
+            lenv = math.sqrt(vector[0] ** 2 + vector[1] ** 2)
+            vector = vector[0] / lenv, vector[1] / lenv
+            if lenv <= radius:
+                vector = 0, 0
+
+            if vector != old:
+                old = vector
+                msg = f"{vector[0]},{vector[1]}"
+                sock.send(msg.encode())
+
+    data = sock.recv(1024).decode()
+    print(data)
+    screen.fill("gray")
+    pygame.draw.circle(screen, (255, 0, 0), CC, radius)
+    drow_text(CC[0],CC[1],radius//2,name,(0,0,0))
+    pygame.display.update()
+pygame.quit()
--- bacteries/server.py
+++ bacteries/server.py
@@ -1,5 +1,67 @@
 import socket
-import time
+
+import pygame
+import psycopg2
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine("postgresql+psycopg2://postgres:1@localhost/bakteriy")
+
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+s = Session()
+
+pygame.init()
+WIDTH_ROOM, HEIGHT_ROOM = (4000, 4000)
+WIDTH_SERVER, HEIGHT_SERVER = (300, 300)
+FPS = 100
+skreem = pygame.display.set_mode((WIDTH_SERVER, HEIGHT_SERVER))
+pygame.display.set_caption("serveer")
+clock = pygame.time.Clock()
+
+
+class Player(Base):
+    __tablename__ = "gamers"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250))
+    address = Column(String)
+    x = Column(Integer, default=500)
+    y = Column(Integer, default=500)
+    size = Column(Integer, default=50)
+    errors = Column(Integer, default=0)
+    abs_speed = Column(Integer, default=1)
+    speed_x = Column(Integer, default=0)
+    speed_y = Column(Integer, default=0)
+    color = Column(String(250), default="red")  # Добавили цвет
+    w_vision = Column(Integer, default=800)
+    h_vision = Column(Integer, default=600)  # Добавили размер квадрат
+
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+# Локальный класс игроков
+class LocalPlayer:
+    def __init__(self, id, name, sock, addr):
+        self.id = id
+        self.db: Player = s.get(Player, self.id)
+        self.sock = sock
+        self.name = name
+        self.address = addr
+        self.x = 500
+        self.y = 500
+        self.size = 50
+        self.errors = 0
+        self.abs_speed = 1
+        self.speed_x = 0
+        self.speed_y = 0
+        self.color = "red"
+        self.w_vision = 800
+        self.h_vision = 600
+
+
+Base.metadata.create_all(engine)
 
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 1.семейство адресов ipv4 2.тип подключения
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY,
@@ -8,19 +70,35 @@ main_socket.bind(("localhost", 10000))  # 4.привзывае подкл к о�
 main_socket.setblocking(False)  # 5 непрерывность,не ожидает ответа при Fals  ждет если True
 main_socket.listen(5)  # 6 прослушка входящих соединений 5 одновремменно
 print("сокет создан")
-players = []
+players = {}
 while True:
+    clock.tick(FPS)
     try:
         new_sock, addr = main_socket.accept()
         print("подключился", addr, new_sock)
         new_sock.setblocking(False)
-        players.append(new_sock)
+        player = Player("олег", addr)
+        s.merge(player)
+        s.commit()
+        addr = f"({addr[0]},{addr[1]})"
+        data = s.query(Player).filter(Player.address == addr)
+        for user in data:
+            player = LocalPlayer(user.id, "олег", new_sock, addr)
+            players[user.id] = player
     except BlockingIOError:
         pass
-    for sock in players:
+    for id in list(players):
         try:
-            data = sock.recv(1024).decode()
+            data = players[id].sock.recv(1024).decode()
             print(data)
         except:
             pass
-    time.sleep(1)
+    for id in list(players):
+        try:
+            players[id].sock.send("фхф".encode())
+        except:
+            players[id].sock.close()
+            del players[id]
+            s.query(Player).filter(Player.id == id).delete()
+            s.commit()
+            print("сокет закрыт")
